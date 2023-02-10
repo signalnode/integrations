1d6cd81ccf7f49f6f3427097ecbfcd296d1a1951
dawosch <57100280+dawosch@users.noreply.github.com>
2023-02-10 09:57:22 +0100

    Properties fixed

--- signalnode-solaredge/src/index.ts
+++ signalnode-solaredge/src/index.ts
@@ -8,7 +8,7 @@ type Config = {
   siteId: string;
 };
 
-type PropertyNames = 'currentExport' | 'currentConsumption' | 'currentSelfConsumption' | 'currentImport';
+type PropertyNames = 'currentExport' | 'currentConsumption' | 'currentSelfConsumption' | 'currentProduction' | 'currentImport';
 
 const SolarEdgeAddon: SignalNodeAddon<Config, PropertyNames> = {
   configLayout: {
@@ -38,31 +38,43 @@ const SolarEdgeAddon: SignalNodeAddon<Config, PropertyNames> = {
   },
   properties: [
     {
+      name: 'currentImport',
+      description: 'Current imported energy',
+      value: 0,
+      unit: 'kW',
+      type: 'Plain',
+      useHistory: true,
+    },
+    {
       name: 'currentExport',
       description: 'Current exported energy',
       value: 0,
-      unit: 'W',
+      unit: 'kW',
+      type: 'Plain',
       useHistory: true,
     },
     {
       name: 'currentConsumption',
       description: 'Current energy consumption',
       value: 0,
-      unit: 'W',
+      unit: 'kW',
+      type: 'Plain',
       useHistory: true,
     },
     {
       name: 'currentSelfConsumption',
-      description: 'Current self energy consumption',
+      description: 'Current energy consumption',
       value: 0,
-      unit: 'W',
+      unit: 'kW',
+      type: 'Plain',
       useHistory: true,
     },
     {
-      name: 'currentImport',
-      description: 'Current imported energy',
+      name: 'currentProduction',
+      description: 'Current self energy consumption',
       value: 0,
-      unit: 'W',
+      unit: 'kW',
+      type: 'Plain',
       useHistory: true,
     },
   ],
@@ -71,29 +83,31 @@ const SolarEdgeAddon: SignalNodeAddon<Config, PropertyNames> = {
       interval: ['*/15', '*', '*', '*', '*'],
       run: async (config: Config) => {
         const res = await solarEdgeClient.getPowerFlow(config.siteId);
-        let currentExport = 0;
-        let currentConsumption = 0;
-        let currentSelfConsumption = 0;
-        let currentImport = 0;
+        const result = {
+          currentImport: 0,
+          currentExport: 0,
+          currentConsumption: 0,
+          currentSelfConsumption: 0,
+          currentProduction: 0,
+        };
 
         res.siteCurrentPowerFlow.connections.forEach((connection) => {
           if (connection.from === 'GRID' && connection.to === 'Load') {
-            currentImport = res.siteCurrentPowerFlow.GRID.currentPower;
+            result.currentImport = res.siteCurrentPowerFlow.GRID.currentPower;
+            result.currentConsumption = res.siteCurrentPowerFlow.LOAD.currentPower;
+          } else if (connection.from === 'LOAD' && connection.to === 'Grid') {
+            result.currentExport = res.siteCurrentPowerFlow.GRID.currentPower;
+            result.currentConsumption = res.siteCurrentPowerFlow.LOAD.currentPower;
+            // currentSelfConsumption = res.siteCurrentPowerFlow.LOAD.currentPower;
           } else if (connection.from === 'PV' && connection.to === 'Load') {
-            currentSelfConsumption = res.siteCurrentPowerFlow.PV!.currentPower;
-
-            const energyExport = res.siteCurrentPowerFlow.PV!.currentPower - res.siteCurrentPowerFlow.LOAD.currentPower;
-            currentExport = energyExport > 0 ? energyExport : 0;
+            result.currentProduction = res.siteCurrentPowerFlow.PV!.currentPower;
+            result.currentSelfConsumption =
+              res.siteCurrentPowerFlow.LOAD.currentPower > res.siteCurrentPowerFlow.PV!.currentPower
+                ? res.siteCurrentPowerFlow.PV!.currentPower
+                : res.siteCurrentPowerFlow.LOAD.currentPower;
           }
         });
-        currentConsumption = res.siteCurrentPowerFlow.LOAD.currentPower;
-
-        return {
-          currentImport,
-          currentConsumption,
-          currentSelfConsumption,
-          currentExport,
-        };
+        return result;
       },
     },
   ],
